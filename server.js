a9434308eaf4292a24894a13cdc475f9a2921b1b
Austin Graves <austingraves106@aol.com>
2019-11-30 19:18:36 -0600

    Added in the server side express-handlebars setup. And ensured the landing route worked to render home

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 // Packages
 const express = require('express');
-
+const exphbs = require('express-handlebars');
 
 // Application Needed Constants
 const app = express();
 const port = 3000 || process.env.PORT;
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+ 
+app.get('/', function (req, res) {
+    res.render('home');
+});
 
 app.listen(port, () => {
   console.log(`Server Started On Port: ${port}`);

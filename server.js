3ca6e6637a39532505fef3270a594aab461615d9
Austin Graves <austingraves106@aol.com>
2019-11-30 19:39:57 -0600

    Added the bulma library, as well as a google font. Started the beginnings of my navbar

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 // Packages
 const express = require('express');
 const exphbs = require('express-handlebars');
-
+const path = require('path');
 // Application Needed Constants
 const app = express();
 const port = 3000 || process.env.PORT;
 
+// Setting the setup for express-handlebars
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
  
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Landing page route
 app.get('/', function (req, res) {
     res.render('home');
 });
